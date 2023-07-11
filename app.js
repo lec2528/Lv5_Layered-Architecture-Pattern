1ba59e26aeb79fbcea7c88866eb06f730a659d17
Lee Eunchan <ym4315@nate.com>
2023-07-11 22:42:10 +0900

    feat:라우터 수정

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = require('./routes');
 
-const postsRouter = require('./routes/posts.js');
-const commentRouter = require('./routes/comments.js');
-const signupRouter = require('./routes/users.js');
-const likesRouter = require('./routes/likes.js');
 const cookieParser = require('cookie-parser');
-
-// 전역객체 body parser에 사용
-
-// 미들웨어
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [postsRouter, commentRouter, signupRouter, likesRouter]);
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
