eb046494d9d1de3bc3af67e9dd6f51f1e29b4693
Lee Eunchan <ym4315@nate.com>
2023-07-12 13:26:14 +0900

    fix: users services 리팩토링 완료

---
+++ services/users.services.js
@@ -0,0 +1,43 @@
+const UsersRepository = require('../repositories/users.repository');
+const jwt = require('jsonwebtoken');
+class UsersService {
+  usersRepository = new UsersRepository();
+
+  createUsers = async (nickname, password, email, confirm) => {
+    const createSignupUserDate = await this.usersRepository.createUsers(
+      nickname,
+      password,
+      email,
+      confirm
+    );
+
+    // if(createSignupUserDate){
+
+    // }
+    return {
+      nickname: createSignupUserDate.nickname,
+      password: createSignupUserDate.password,
+      email: createSignupUserDate.email,
+      confirm: createSignupUserDate.confirm,
+      userId: createSignupUserDate.confirm,
+    };
+  };
+  doLogin = async (nickname, password) => {
+    const user = await this.usersRepository.findByUsers(nickname, password);
+
+    if (user) {
+      // 로그인 성공
+      const userInfo = {
+        nickname: user.nickname,
+      };
+      // 토큰을 생성합니다.
+      const token = jwt.sign({ userId: user.userId }, 'custom-secret-key');
+      // 토큰을 반환합니다
+      return { userInfo, token };
+    } else {
+      // 로그인 실패
+      throw new Error('아이디 또는 비밀번호를 확인해주세요');
+    }
+  };
+}
+module.exports = UsersService;
