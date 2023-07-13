4568a99ecaad1e04b0cc423db539e8c8e08cd8c6
Lee Eunchan <ym4315@nate.com>
2023-07-13 21:25:17 +0900

    feat:comments 라우터  추가

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
 const userRouter = require('./users.routes');
-router.use('/users', userRouter);
-
 const postsRouter = require('./posts.routes');
-router.use('/post', postsRouter);
-
 const commentsRouter = require('./comments.routes');
+// const commentsRouter = require('./comments.routes');
+
+router.use('/user', userRouter);
+router.use('/post', postsRouter);
 router.use('/comment', commentsRouter);
+// router.use('/comment', commentsRouter);
 module.exports = router;
