e895282db62ffbc5b21d8d75520eb7896c2c5104
Lee Eunchan <ym4315@nate.com>
2023-07-13 10:20:55 +0900

    fix:테스트 중 발생한 에러로 인한 일부 코드 수정 postId를 못받아오는 상황이었음 console.log 찍어보고 확인하기

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -10,24 +10,27 @@ class PostRepository {
     return posts;
   };
 
-  findOnePost = async () => {
+  findOnePost = async (postId) => {
     const detailPost = await Posts.findOne({ where: { postId } });
+    console.log(detailPost);
     return detailPost;
   };
-  createPost = async (nickname, password, title, content) => {
+  createPost = async (nickname, password, title, content, userId, postId) => {
     // ORM인 Sequelize에서 Posts 모델의 create 메소드를 사용해 데이터를 요청합니다.
     const createPostData = await Posts.create({
+      postId,
       nickname,
       password,
       title,
       content,
+      userId,
     });
 
     return createPostData;
   };
-  updatePost = async (title, content, updatedAt) => {
+  updatePost = async (postId, title, content) => {
     const updatePostData = await Posts.update(
-      { title, content },
+      { title, content, updatedAt: new Date() },
       { where: { postId } }
     );
     return updatePostData;
