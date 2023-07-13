1957e1cc5b30a722865f9e190f64124943629f0e
Lee Eunchan <ym4315@nate.com>
2023-07-13 10:18:36 +0900

    fix:테스트 중 발생한 에러로 인한 일부 코드 수정 postId를 못받아오는 상황이었음

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -6,9 +6,11 @@ const router = express.Router();
 const PostsController = require('../controllers/posts.controller.js');
 const postsController = new PostsController();
 
+const middleware = require('../Middleware/loginmiddleware');
+
 router.get('/', postsController.getPosts);
 router.get('/:postId', postsController.getPostById);
-router.post('/', postsController.createPost);
-router.put('/:postId', postsController.updatePost);
-router.delete('/:postId', postsController.deletePost);
+router.post('/', middleware, postsController.createPost);
+router.put('/:postId', middleware, postsController.updatePost);
+router.delete('/:postId', middleware, postsController.deletePost);
 module.exports = router;
