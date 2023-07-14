c3e4d2aad8e7e066d0a24615343eb2f2733fc0a2
Lee Eunchan <ym4315@nate.com>
2023-07-14 18:27:51 +0900

    feat:코멘트 리팩토링 중간 업로드 외래키 제약조건 이슈 발생

--- services/comments.services.js
+++ services/comments.services.js
@@ -2,4 +2,82 @@ const CommentsRepository = require('../repositories/comments.repository');
 
 class CommentsService {
   commentsRepository = new CommentsRepository();
+
+  getComments = async () => {
+    const comments = await this.commentsRepository.findByCommentsToPost();
+    if (!comments) {
+      throw new Error('조회 가능한 댓글 없습니다.');
+    }
+
+    comments.sort((a, b) => {
+      return b.createdAt - a.createdAt;
+    });
+
+    // 비즈니스 로직을 수행한 후 사용자에게 보여줄 데이터를 가공합니다.
+    return comments.map((comment) => {
+      return {
+        userId: comment.userId,
+        postId: comment.postId,
+        nickname: comment.nickname,
+        title: comment.title,
+        createdAt: comment.createdAt,
+        updatedAt: comment.updatedAt,
+      };
+    });
+  };
+
+  createComments = async (userId, postId, nickname, content) => {
+    await this.commentsRepository.createComments(
+      userId,
+      postId,
+      nickname,
+      content
+    );
+    const createCommentsData = await this.postRepository.findOnePost(postId);
+    return {
+      userId: createCommentsData.userId,
+      postId: createCommentsData.postId,
+      nickname: createCommentsData.nickname,
+      content: createCommentsData.content,
+    };
+  };
+  updateComments = async (commentId, userId, postId, content, updateAt) => {
+    const findComment = await this.commentsRepository.findOneComment(commentId);
+
+    if (!findComment) {
+      throw new Error('존재하지 않는 게시글 입니다.');
+    }
+
+    if (!findComment) {
+      throw new Error('수정할 내용을 입력해주세요');
+    }
+    await this.commentsRepository.updateComments(
+      userId,
+      postId,
+      content,
+      updateAt
+    );
+    const updateCommentsData = await this.commentsRepository.findOneComment(
+      postId
+    );
+    return {
+      userId: updateCommentsData.userId,
+      postId: updateCommentsData.postId,
+      content: updateCommentsData.content,
+      updateAt: updateCommentsData.updateAt,
+    };
+  };
+  deleteComments = async (commentId) => {
+    const findPost = await this.postRepository.findOnePost(commentId);
+    await this.postRepository.deletePost(commentId);
+    return {
+      commentId: findPost.commentId,
+      postId: findPost.postId,
+      nickname: findPost.nickname,
+      title: findPost.title,
+      content: findPost.content,
+      updatedAt: findPost.updatedAt,
+    };
+  };
 }
+module.exports = CommentsService;
